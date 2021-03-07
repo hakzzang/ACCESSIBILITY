b3c4ee598ddd168676941d8a659b966c266f1fb5
hakzzang <mbh930312@naver.com>
2021-03-07 22:46:40 +0900

    feat : merge 작업

---
+++ app/src/main/java/com/hbs/myaccessibility/MergeFocusActivity.kt
@@ -0,0 +1,11 @@
+package com.hbs.myaccessibility
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MergeFocusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_merge_focus)
+    }
+}
